a58665a3acb27377b7b912809221a8aeb8fac322
Tao Chen <enzocxt@gmail.com>
2020-05-29 15:27:20 +0200

    add: api create_user()

--- back-end/app/api/users.py
+++ back-end/app/api/users.py
@@ -1,10 +1,48 @@
+import re
+from flask import (
+    request,
+    jsonify,
+    url_for,
+)
+from app import db
 from app.api import bp
+from app.api.auth import token_auth
+from app.api.errors import bad_request
+from app.models import User
 
 
 @bp.route('/users', methods=['POST'])
 def create_user():
     """注册一个新用户"""
-    ...
+    data = request.get_json()
+    if not data:
+        return bad_request('You must post JSON data!')
+
+    message = {}
+    if 'username' not in data or not data.get('username', None):
+        message['username'] = 'Please provide a valid username.'
+    pattern = '^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$'
+    if 'email' not in data or not re.match(pattern, data.get('email', None)):
+        message['email'] = 'Please provide a valid email address.'
+    if 'password' not in data or not data.get('password', None):
+        message['password'] = 'Please provide a valid password.'
+
+    if User.query.filter_by(username=data.get('username', None)).first():
+        message['username'] = 'Please use a different username.'
+    if User.query.filter_by(email=data.get('email', None)).first():
+        message['email'] = 'Please use a different email address.'
+    if message:
+        return bad_request(message)
+
+    user = User()
+    user.from_dict(data, new_user=True)
+    db.session.add(user)
+    db.session.commit()
+    response = jsonify(user.to_dict())
+    response.status_code = 201
+    # HTTP 协议要求 201 响应包含一个值为新资源 URL 的 Location 头部
+    response.headers['Location'] = url_for('api.get_user', id=user.id)
+    return response
 
 
 @bp.route('/users', methods=['GET'])
