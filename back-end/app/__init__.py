9d3c6f6ad6f6d27b921d707d10fd3396587c983a
Tao Chen <enzocxt@gmail.com>
2020-05-29 10:50:20 +0200

    trivial

--- back-end/app/__init__.py
+++ back-end/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 from config import Config
 
 
@@ -6,6 +7,11 @@ from config import Config
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
+
+    # 前端地址是 localhost:8080，而后端是 5000
+    # 所以前端发来的 ajax 请求属于跨域请求
+    # enable CORS
+    CORS(app)
 
     # register blueprint
     from app.api import bp as api_bp
